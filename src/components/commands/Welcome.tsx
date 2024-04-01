0adb4c709a15c41b27ebafad8e00224f0e15b6a5
gpradhan07 <91816386+gpradhan07@users.noreply.github.com>
2024-04-01 14:33:43 +0530

    minor chnages

--- src/components/commands/Welcome.tsx
+++ src/components/commands/Welcome.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Welcome() {
 	return (
 		<div>
-			<h1 className=" font-fascinate text-4xl font-bold uppercase">Welcome to my protfolio</h1>
+			<h1 className=" font-fascinate text-4xl font-bold uppercase">Welcome to my profile</h1>
 		</div>
 	);
 }
